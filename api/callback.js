baef3ed38192263930c943d43f438b72fa44aa4c
skdidbstkd <skdidbstkd@gmail.com>
2025-10-18 16:12:54 +0900

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
     var token  = ${JSON.stringify(token)};
     window.opener && window.opener.postMessage(
       'authorization:github:success:' + token,
-      origin
+      '*'
     );
     window.close();
   })();
