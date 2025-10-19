9c590cc13889b92425b9ba8ec6f76de6f638c47e
skdidbstkd <skdidbstkd@gmail.com>
2025-10-19 21:25:03 +0900

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,11 +1,6 @@
 // api/auth.js
 import { github } from 'decap-cms-oauth-provider-node';
 
-// ✅ Vercel 런타임 설정
-export const config = {
-  runtime: 'nodejs'
-};
-
 export default github({
   client_id: process.env.OAUTH_CLIENT_ID,
   client_secret: process.env.OAUTH_CLIENT_SECRET
